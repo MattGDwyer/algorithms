876d7e361792d99f451d174f08b3abf7557d4614
Matthew Dwyer <matthewgdwyer@gmail.com>
2020-11-15 18:38:54 -0800

    First Commit

---
+++ reverseString.js
@@ -0,0 +1,8 @@
+// arrays
+
+// question 1) fill out this function so that it returns a reverse string, do not use ANY common functions that just do it for you
+function reverseString(inputString) {
+
+}
+var x = "Legolas";
+console.log(reverseString(x));
