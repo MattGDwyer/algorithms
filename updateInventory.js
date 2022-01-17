fec88368a2069b072aaf2a6c515c3790aa9d6c6f
Matthew Dwyer <matthewgdwyer@gmail.com>
2022-01-17 13:51:11 +0700

    updateInventory completed

---
+++ updateInventory.js
@@ -0,0 +1,58 @@
+function updateInventory(arr1, arr2) {
+  // slice arr1 so as not to mutate it.
+  let updatedInventory = arr1.slice();
+
+  // edge case for empty beginning inventory
+  if (arr1.length === 0) {
+
+      return arr2.slice().sort(compareIndexOne);
+  }
+
+  let counter = 0;
+  for (let i = 0; i < arr2.length; i++) {
+      for (let j = 0; j < updatedInventory.length; j++) {
+          if (arr2[i][1] === updatedInventory[j][1]) {
+              let newValue = arr2[i][0] + updatedInventory[j][0];
+              delete updatedInventory[j][0];
+              updatedInventory[j][0] = newValue;
+
+          } else {
+              counter += 1;
+          }
+          // if all items in updatedInventory have been checked
+          if (counter === updatedInventory.length) {
+              updatedInventory.push(arr2[i]);
+              // stop loop so we don't double the amount of the last item
+              break;
+          }
+      }
+      counter = 0;
+  }
+  return updatedInventory.sort(compareIndexOne);
+}
+// helper function to alphabetize entries
+function compareIndexOne(a, b) {
+  if (a[1] === b[1]) {
+      return 0;
+  }
+  else {
+      return (a[1] < b[1]) ? -1 : 1;
+  }
+}
+
+// Example inventory lists
+var curInv = [
+  [21, "Bowling Ball"],
+  [2, "Dirty Sock"],
+  [1, "Hair Pin"],
+  [5, "Microphone"]
+];
+
+var newInv = [
+  [2, "Hair Pin"],
+  [3, "Half-Eaten Apple"],
+  [67, "Bowling Ball"],
+  [7, "Toothpaste"]
+];
+
+updateInventory(curInv, newInv);
